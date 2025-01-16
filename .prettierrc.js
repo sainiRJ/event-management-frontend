7b7476eeff715c16b7381fbb9b6adbe816e42256
saurabh.kumar <saurabh.kumar@softway.com>
2025-01-16 19:00:04 +0530

    feat: initial project setup

---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+    semi: true,
+    arrowParens: "always",
+    bracketSpacing: false,
+    bracketSameLine: false,
+    singleQuote: false,
+    trailingComma: "all",
+    useTabs: true,
+  };
+  
