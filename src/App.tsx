1b1a49a17c5c057bb45de0ae55b8372951d51e34
saurabh.kumar <saurabh.kumar@softway.com>
2025-01-20 19:20:06 +0530

    feat update navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,31 @@
 import React from "react";
+import {Button, CustomProvider, Container} from "rsuite";
+import "rsuite/dist/rsuite.min.css";
+import HeaderTab from "./components/layouts/Header";
+import SidebarUI from "./components/layouts/Sidebar";
 
 function App() {
+	console.log("App");
 	return (
-		<div className="App">
-			<header className="App-header">
-				<p>
-					Edit <code>src/App.tsx</code> and save to reload.
-				</p>
-				<a
-					className="App-link"
-					href="https://reactjs.org"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					Learn React
-				</a>
-			</header>
-		</div>
+		<CustomProvider theme="light">
+			<Container className="app">
+				<HeaderTab />
+				<SidebarUI />
+				<header className="app-header">
+					<p>
+						Edit <code>src/App.js</code> and save to reload.
+					</p>
+
+					<Button
+						href="https://reactjs.org"
+						target="_blank"
+						rel="noopener noreferrer"
+					>
+						Learn React
+					</Button>
+				</header>
+			</Container>
+		</CustomProvider>
 	);
 }
 
---
+++ src/components/layouts/Header.tsx
@@ -0,0 +1,39 @@
+import React from "react";
+
+import {Navbar, Nav, Button, Dropdown, Whisper} from "rsuite";
+import AdminIcon from "@rsuite/icons/Admin";
+import NoticeIcon from "@rsuite/icons/Notice";
+import HeaderPopover from "../HeaderPopover/HeaderPopover";
+
+const HeaderTab = () => (
+	<>
+		<Navbar>
+			<Navbar.Brand href="#">RSUITE</Navbar.Brand>
+			<Nav pullRight>
+				<Whisper
+					placement="bottomEnd"
+					trigger="click"
+					controlId="profile-popover"
+					preventOverflow
+					speaker={<HeaderPopover arrow={false} />}
+				>
+					<Nav.Item icon={<NoticeIcon />}></Nav.Item>
+				</Whisper>
+				<Dropdown
+					title={
+						<span>
+							<AdminIcon /> Profile
+						</span>
+					}
+					noCaret
+					placement="bottomEnd"
+				>
+					<Dropdown.Item> Account Settings</Dropdown.Item>
+					<Dropdown.Item>Logout</Dropdown.Item>
+				</Dropdown>
+			</Nav>
+		</Navbar>
+	</>
+);
+
+export default HeaderTab;
