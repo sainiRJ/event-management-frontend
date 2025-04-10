06ab387728cfdc3ffa392a59ab61fa5c23d73636
sainiRJ <rajeshpushpakar01@gmail.com>
2025-04-10 19:07:26 +0530

    fix: endpoint issue

--- src/config/index.ts
+++ src/config/index.ts
@@ -4,7 +4,8 @@ import featureFlagConfig from "./featureFlagConfig";
  * Environment variables used in the application
  */
 const env = {
-	EVENT_MANAGEMENT_BASE_URL: process.env.REACT_APP_EVENT_MANAGEMENT_BACKEND_BASEURL,
+	EVENT_MANAGEMENT_BASE_URL:
+		process.env.REACT_APP_EVENT_MANAGEMENT_BACKEND_BASEURL,
 	EVENT_MANAGEMENT_API_SERVER_TIMEOUT:
 		process.env.EVENT_MANAGEMENT_API_SERVER_TIMEOUT,
 
